b4ec0ae414e7aad9b514fc840d19e77ad0861981
foyzur17 <foyzur17@gmail.com>
2022-05-23 21:08:46 +0600

    Add Data

---
+++ lib/screen/products_overview_screen.dart
@@ -0,0 +1,18 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class ProductOverviewScreen extends StatelessWidget {
+  const ProductOverviewScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(),
+      body: Text("Flutter overview screen"),
+
+    );
+    
+  }
+}
